3ffc448fa4769927899d94bfe72ee7b6c5d32030
saradaabura <raspico@outlook.jp>
2025-01-06 17:14:48 +0900

    Update log.py

--- log.py
+++ log.py
@@ -1,1 +1,11 @@
-
+path = "/gps.txt" ###full path
+savespeed = 10 #defalt
+def wr(data):
+    #led.value(1) confirm
+    with open(path, 'a') as f:
+        f.write(str(data) + '\n')
+    #led.value(0)
+while True:
+  if time.time() - ti >= savespeed:
+    wr(temp)
+    ti = time.time()
